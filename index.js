c7101d1fb807464eb8c4f510500fd2a2aab9bfed
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-07 01:14:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var sentences = [];
+  for(var i = 0; i < musicians.length; i++) {
+    sentences.push(musicians[i] + ` plays ${instruments[i]}`);
+  }
+  return sentences;
+}
+
+function johnLenonFacts(facts) {
+  
+}
